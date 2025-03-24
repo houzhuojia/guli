8bcfb7dce1fc7e72c33374ddecdc16c5955616b9
houzhuojia <3321684846@qq.com>
2025-03-24 12:24:36 +0800

    Update README.md 第一次提交 项目搭建

---
+++ mall-coupon/src/test/java/com/hzj/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzj.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
